b59f3ecd2a01e1bf2c7362a11bce73499880b177
Charbel <charbel.jalkh@hotmail.com>
2020-12-01 22:52:52 +1100

    Update to Laravel 8.x

--- app/User.php
+++ app/User.php
@@ -3,12 +3,14 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
